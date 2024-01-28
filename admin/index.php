09b9762b3a7f261685907e71c7375b8a68b5ed12
tuanzang <tuanldph26359@fpt.edu.vn>
2024-01-28 22:29:22 +0700

    duan

---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php 
+    include 'header.php';
+    include 'list.php';
+    include 'sidebar.php';
+?>
---
+++ admin/sidebar.php
@@ -0,0 +1,85 @@
+
+    <!-- SIDEBAR -->
+    <section id="sidebar">
+        <a href="#" class="brand">
+            <img id="logo" src="images_giao_dien/logo.png" alt="">
+            <span class="text">Admin</span>
+        </a>
+        <ul class="side-menu top">
+            <li class="">
+                <a href="#">
+                    <i class='bx bxs-dashboard'></i>
+                    <span class="text">Quản Lý Sản Phẩm</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-food-menu'></i>
+                    <span class="text">Quản Lý Danh Mục Sản Phẩm</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-shopping-bag-alt'></i>
+                    <span class="text">Quản Lý Đơn Hàng</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-group'></i>
+                    <span class="text">Quản Lý User</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-message-dots'></i>
+                    <span class="text">Quản Lý Bình Luận</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-doughnut-chart'></i>
+                    <span class="text">Thống Kê</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="side-menu">
+            <li>
+                <a href="#">
+                    <i class='bx bxs-cog'></i>
+                    <span class="text">Cài đặt</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="logout">
+                    <i class='bx bxs-log-out-circle'></i>
+                    <span class="text">Thoát</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- <script>
+            // const allSideMenu = document.querySelectorAll('#sidebar .side-menu .top li a');
+
+            // allSideMenu.forEach(item => {
+            // 	const li = item.parentElement;
+
+            // 	item.addEventListener('click', function() {
+            // 		allSideMenu.forEach(i => {
+            // 			i.parentElement.classList.remove('active');
+            // 		})
+            // 		li.classList.add('active');
+            // 	})
+            // });
+
+        </script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script> -->
